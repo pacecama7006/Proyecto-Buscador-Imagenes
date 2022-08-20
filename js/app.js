34771ad164eb438f948645e652b92d63deb241e1
Paulo <pacecama7006@gmail.com>
2022-08-20 07:17:37 -0500

    Se crea la funcionalidad para mostrar una alerta en el html, en caso de que el usuario de click en buscar imágenes y no haya capturado un término de búsqueda

---
+++ js/app.js
@@ -0,0 +1,46 @@
+import { apiKey } from "./env.js";
+
+// Variables del htlm
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+window.onload = () => {
+    formulario.addEventListener('submit', validarFormulario);
+};
+
+function validarFormulario(e) {
+    e.preventDefault();
+
+    const terminoBusqueda = document.querySelector('#termino').value;
+
+    if (terminoBusqueda === '') {
+        // console.log('Agrega un término de búsqueda');
+        mostrarAlerta('Agrega un término de búsqueda');
+        return;
+    }
+}
+// Fin validarFormulario
+
+function mostrarAlerta(mensaje) {
+
+    const existeAlerta = document.querySelector('.bg-red-100');
+
+    if (!existeAlerta) {
+        const alerta = document.createElement('p');
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3','rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+
+        alerta.innerHTML = `
+            <strong class="font-bold">Error!</strong>
+            <span class="block sm:inline">${mensaje}</span>
+        `;
+
+        formulario.appendChild(alerta);
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+
+    }
+    
+}
+// Fin mostrar alerta
