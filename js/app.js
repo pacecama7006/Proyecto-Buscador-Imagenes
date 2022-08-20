29eec477c91242402c3197fd692f574899b48fd6
Paulo <pacecama7006@gmail.com>
2022-08-20 08:46:10 -0500

    Se realiza la funcionalidad para obtener las imágenes de la api, y se muestran en el html

--- js/app.js
+++ js/app.js
@@ -18,6 +18,9 @@ function validarFormulario(e) {
         mostrarAlerta('Agrega un término de búsqueda');
         return;
     }
+
+    buscarImagenes(terminoBusqueda);
+
 }
 // Fin validarFormulario
 
@@ -44,3 +47,60 @@ function mostrarAlerta(mensaje) {
     
 }
 // Fin mostrar alerta
+
+function buscarImagenes(termino) {
+    // console.log(termino);
+
+    const key = apiKey;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+
+    console.log(url);
+
+    fetch(url)
+        .then(resultado =>{
+            return resultado.json();
+        })
+        .then(datos => {
+            
+            // console.log(datos.hits);
+            mostrarImagenes(datos.hits);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+// Fin buscar imagenes
+
+function mostrarImagenes(imagenes) {
+    // console.log(imagenes);
+    limpiarHtml();
+
+    // Iteramos sobre el arreglo de imágenes y construir el html
+    imagenes.forEach(imagen => {
+        const {pageURL, previewURL, likes, views, webformatURL, largeImageURL}= imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}">
+                    <div class="p-4">
+                        <p class="font-bold">${likes} <span class="font-light">Me gusta</span></p>
+                        <p class="font-bold">${views} <span class="font-light">Vistas</span></p>
+                        <a 
+                            class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                            href="${largeImageURL}" target="_blank" rel="noopener noreferrer">Vista previa</a>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+
+
+}
+// Fin mostrar imagenes
+
+function limpiarHtml() {
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
